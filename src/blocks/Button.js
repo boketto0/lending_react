40b1c61b861b33e1dff65ccde789015e92447237
boketto <bekmina.julia@gmail.com>
2023-03-23 17:38:46 +0300

    map, componenets

---
+++ src/blocks/Button.js
@@ -0,0 +1,20 @@
+import "./Button.css";
+
+function Button(props) {
+    const items = [
+        {
+            text: 'Задать вопрос',
+        },
+        {
+            text: 'Все компоненты',
+        },
+        {
+            text: 'Все компоненты',
+        },
+        {
+            text: 'Задать вопрос',
+        },
+    ]
+}
+
+export default Button;
