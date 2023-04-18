184280efde942a5d55688ca326416b6a7c728037
boketto <bekmina.julia@gmail.com>
2023-04-18 21:22:58 +0300

    button

--- src/blocks/Components/Button/Button.js
+++ src/blocks/Components/Button/Button.js
@@ -1,24 +1,24 @@
 import "./Button.css";
 
-const ButtonTextType = {
+export const ButtonType = {
   PRIMARY: "primary",
   SECONDARY: "secondary",
   LINE: "line"
 };
 
-let Button = (props) => {
+export const Button = (props) => {
 
   return(
     <div className={`
         button button-${props.type}
         `}>
-        {props.textType === ButtonTextType.PRIMARY && (
+        {props.textType === ButtonType.PRIMARY && (
           <div className="button-text">{props.text}</div>
         )}
-        {props.textType === ButtonTextType.SECONDARY && (
+        {props.textType === ButtonType.SECONDARY && (
           <div className="button-text__border">{props.text}</div>
         )}
-        {props.textType === ButtonTextType.LINE && (
+        {props.textType === ButtonType.LINE && (
           <div className="button-line__text">{props.text}</div>
         )}
         {props.icon && <div className="button-icon">{props.icon}</div>} 
@@ -26,6 +26,6 @@ let Button = (props) => {
   )
 
 }
-export {Button, ButtonTextType};
+
 
 // передавать пропсом саму иконку, док react, импортировать svg как компонент
